47d8d4940ef16558132c055690801d2ec5437e26
Jean-Baptiste Lasselle <jblasselle@michelin.io>
2023-02-05 01:22:35 +0100

    add certbot

--- rstudio/containers/.prepare/.prepare.sh
+++ rstudio/containers/.prepare/.prepare.sh
@@ -4,6 +4,19 @@ ls -alh ./.run/.*/.*.sh && chmod +x ./.run/.*/.*.sh
 ls -alh ./.prepare/.*/.*.sh && chmod +x ./.prepare/.*/.*.sh
 chmod +x ./.prepare/.create.volumes.folders.sh
 ./.prepare/.create.volumes.folders.sh
+
+echo
+echo "# --- # --- # --- "
+echo "# --- Volumes Folders are now created."
+echo "# --- # --- # --- "
+echo
+
+
+echo
+echo "# --- # --- # --- "
+echo "# --- Now initializing nginx/letsencrypt."
+echo "# --- # --- # --- "
+echo
 # ./.certbot/
 chmod +x ./.prepare/.nginx/init-letsencrypt.sh
 sudo ./.prepare/.nginx/init-letsencrypt.sh
